49f953d0313b88ec47b99c0f38554a75cf3d8719
olegveshchykov <ipz222_vom@student.ztu.edu.ua>
2025-11-12 15:45:37 +0200

    додано 3 завдання

--- lab2/SurveySln/SurveyPortal/Models/Question.cs
+++ lab2/SurveySln/SurveyPortal/Models/Question.cs
@@ -1,17 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SurveyPortal.Models
 {
     public class Question
     {
         public long QuestionID { get; set; }
+
+        [Required(ErrorMessage = "Будь ласка, введіть текст питання")]
+        [Display(Name = "Текст питання")]
         public string Text { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Будь ласка, оберіть тип питання")]
+        [Display(Name = "Тип питання")]
         public string QuestionType { get; set; } = "Text";
 
-        // --- Зв'язок Один-до-Багатьох ---
-
-        // 1. Foreign Key (Зовнішній ключ)
+        [Required]
         public long SurveyID { get; set; }
 
-        // 2. Navigation Property (Навігаційна властивість до "одного")
         public Survey? Survey { get; set; }
     }
 }
--- lab2/SurveySln/SurveyPortal/Models/Survey.cs
+++ lab2/SurveySln/SurveyPortal/Models/Survey.cs
@@ -1,17 +1,33 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-namespace SurveyPortal.Models
 
+namespace SurveyPortal.Models
 {
     public class Survey
     {
-        public long SurveyID { get; set; }     
-        public string Title { get; set; } = "";
-        public string Description { get; set; } = "";
-        public string Creator { get; set; } = "";
-        public decimal AverageRating { get; set; }  
-        public string Category { get; set; } = "";
+        public long SurveyID { get; set; }
 
-        // Зв'язок Один-до-Багатьох
+        [Required(ErrorMessage = "Будь ласка, введіть назву опитування")]
+        [Display(Name = "Назва")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Будь ласка, введіть опис")]
+        [Display(Name = "Опис")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Будь ласка, введіть автора")]
+        [Display(Name = "Автор")]
+        public string Creator { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Будь ласка, введіть категорію")]
+        [Display(Name = "Категорія")]
+        public string Category { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0.01, 5.00, ErrorMessage = "Рейтинг має бути між 0.01 та 5.00")]
+        [Column(TypeName = "decimal(8, 2)")]
+        [Display(Name = "Середній рейтинг")]
+        public decimal AverageRating { get; set; }
         public List<Question> Questions { get; set; } = new List<Question>();
     }
 }
